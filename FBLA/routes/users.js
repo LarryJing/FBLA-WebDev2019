313fd4da7fd271c7c63794fdb4f58e1060692894
Larry <boaboaj@gmail.com>
2019-02-09 12:55:20 -0800

    basic website up
    got express to work, landing page, added navbar to layout

--- FBLA/routes/users.js
+++ FBLA/routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+const router = express.Router();
+// const User = require('../models/user');
+//
+// // User New
+// router.get('/new', (req, res) => {
+//   res.render('users/new', {err: req.query.err});
+// });
+//
+// // User create
+// router.post('/', (req, res) => {
+//   //
+//   const user = new User(req.body);
+//   User.find({username: user.username}, function(err, user1) {
+//     if (user1 == '' || !user1) {
+//       user.save(function(err, user) {
+//         if(err) return res.redirect(`/users/new?err=${err}`);
+//         return res.redirect('/login');
+//       });
+//     }
+//     else {
+//       res.render('users/new', { message: "Username is taken"});
+//     }
+//   });
+// });
+//
 module.exports = router;
