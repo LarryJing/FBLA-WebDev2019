535e1c4cfc34c2142f09cb2a2de9a640fb4b8ecf
Larry <boaboaj@gmail.com>
2019-02-10 14:17:58 -0800

    big changes, base website
    nav bar, login, carousel, calendar

--- FBLA/routes/index.js
+++ FBLA/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Accuracy' });
 });
 
+// login
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// POST login
+router.post('/login', (req, res, next) => {
+  User.authenticate(req.body.username, req.body.password, (err, user) => {
+    if (err || !user) {
+      const next_error = new Error("Username or password incorrect");
+      next_error.status = 401;
+
+      return res.render('login', { message: "Username or password incorrect"});
+    } else {
+      req.session.userId = user._id;
+      return res.redirect('/') ;
+    }
+  });
+});
+
 module.exports = router;
