0cf2d1922986087d47ab5e1382f5dabce4d64df9
zwang <zachary.wang@gmail.com>
2019-05-08 00:25:33 -0700

    animation fix for realz

--- FBLA/js/script.js
+++ FBLA/js/script.js
@@ -1,12 +1,16 @@
+var navstatus = true;
+
 $(document).ready(function() {
 
     $('.link-button').click(function() {
         $('#splash').fadeOut("slow");
     });
 
-    if($(".navbar").offset().top > 0) {
+    if($(".navbar").offset().top > 0 && navstatus) {
         $('.navbar .wow').each(function(){
             $(this).addClass('menu-fix');
+            $(this).removeClass('bounceInUp');
+            navstatus = false;
         });
     }
 
